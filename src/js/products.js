2166f721727dde0c04d90eb231b08bb1047ce0ea
Lemme.code <djam4343@gmail.com>
2024-03-13 12:31:47 +0500

    @core/features your products section working (functionaly)

--- src/js/products.js
+++ src/js/products.js
@@ -5,7 +5,6 @@ import {
     collection,
     getDocs
 } from "firebase/firestore";
-import { getStorage } from "firebase/storage";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -24,13 +23,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
-
-// const imgs = [
-//   "https://cdn.shopify.com/s/files/1/0554/8176/1895/files/4f30bb6a33538a1164b815f9f93fa13c_720x720.jpg",
-//   "https://cdn.shopify.com/s/files/1/0554/8176/1895/files/eda83abdf052a7b8f852bc060ba6dad4_720x720.jpg",
-//   "https://cdn.shopify.com/s/files/1/0554/8176/1895/files/fc92d965dddc22dd30f91e19aff450f0_720x720.jpg",
-// ]
-// console.log(JSON.stringify(imgs))
 
 //reading data from db
 const doc_ref = collection(db, "Top-products");
