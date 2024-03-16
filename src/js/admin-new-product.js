c4022b12f1f3c00b983aa3e733a3a6fa4dc3ab08
Lemme.code <djam4343@gmail.com>
2024-03-16 17:10:54 +0500

    @core/features product image drag n drop enhanced

--- src/js/admin-new-product.js
+++ src/js/admin-new-product.js
@@ -16,6 +16,7 @@ const preventDefaults = function(e) {
   e.preventDefault()
   e.stopPropagation()
 }
+// to handle files 
 const handleFiles = function(files, dropArea){
   ([...files]).forEach(file => {
     previewFile(file, dropArea)
@@ -27,20 +28,31 @@ const handleDrop = function(e, areaElem){
   const files = data.files
   handleFiles(files, areaElem)
 }
+//previwing file before uploading them
 const previewFile = function(file, dropArea){
-  console.log(``,dropArea)
+  // elems the need to be hiden when preview img is displayed
    const hidePreviewElem = dropArea.querySelectorAll('[data-hideOnImagePreview]');
+  // elems the need to be visible when preview img is displayed
+   const showPreviewElem = dropArea.querySelectorAll('[data-showOnImagePreview]');
+   //hiding those elems
    hidePreviewElem.forEach((elem)=>{
     elem.classList.add('hidden')
    })
+   //make visible those elems
+   showPreviewElem.forEach((elem)=>{
+    elem.classList.remove('hidden')
+   })
+  //  elem where img will be displayed 
    const previewDisplay = dropArea.querySelector('.preview-img')
+   //reading the file using filereader api to display it
     let reader = new FileReader();
     reader.readAsDataURL(file);
+
     reader.onloadend = function(){
-      previewDisplay.classList.remove('hidden')
       previewDisplay.src = reader.result
     }
 }
+// to make the drop area default once clicked cancel btn 
 // to highlight the drop area 
 const highlight = function(elem){
   elem.classList.add('highlight')
@@ -49,6 +61,7 @@ const highlight = function(elem){
 const unhighlight = function(elem){
   elem.classList.remove('highlight')
 }
+
 
 // eventlistners
 primary_image_upload.addEventListener("change",(e)=>{
@@ -60,32 +73,37 @@ primary_image_upload.addEventListener("change",(e)=>{
 // primary_image_upload.addEventListener("mouseover")
 //to prevent (Drag n drop) default actions
 imageDropAreas.forEach((areaElem)=>{
+  // events 
   const preventDefaultEvents = ['dragenter', 'dragover', 'dragleave', 'drop'];
   const highlightEvents = ['dragenter', 'dragover'];
   const unHighlightEvents = ['dragleave', 'drop'];
 
   //preventing defaults
-  for(let i=0; i<preventDefaultEvents.length; i++){
-    areaElem.addEventListener(preventDefaultEvents[i],preventDefaults)
-  }
+  preventDefaultEvents.forEach(event => {
+    areaElem.addEventListener(event, preventDefaults)
+  });
 
   // highlighting 
-  for (let i = 0; i < highlightEvents.length; i++) {
-    areaElem.addEventListener(highlightEvents[i],()=>{
+  highlightEvents.forEach(event => {
+    areaElem.addEventListener(event, ()=>{
       highlight(areaElem)
     })
-  }
-  // unhighlighting 
-  for (let i = 0; i < unHighlightEvents.length; i++) {
-    areaElem.addEventListener(unHighlightEvents[i],()=>{
+  })
+  // unhighlighting
+  unHighlightEvents.forEach(event =>{
+    areaElem.addEventListener(event, ()=>{
       unhighlight(areaElem)
     })
-  }
+  }) 
 
   // once something is dropped
   areaElem.addEventListener('drop', (e)=>{
     handleDrop(e,areaElem)
   }, false)
 
+  //removing img (cancel) btn eventlistners
+  areaElem.addEventListener('click',()=>{
+    areaElem.querySelector('.input_file').click()
+  })
 })
   
