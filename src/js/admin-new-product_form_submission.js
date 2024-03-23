592b924cff5dcb99da9be17a36372ab012031582
Lemme.code <djam4343@gmail.com>
2024-03-23 12:20:15 +0500

    production

--- src/js/admin-new-product_form_submission.js
+++ src/js/admin-new-product_form_submission.js
@@ -1,14 +1,22 @@
 "use strict";
 // essential imports 
 import {initializeApp} from "firebase/app"
-import { getStorage, ref, uploadBytes,uploadBytesResumable, getDownloadURL } from "firebase/storage";
+// import { 
+//     getStorage, 
+//     ref, 
+//     uploadBytes,
+//     uploadBytesResumable, 
+//     getDownloadURL 
+// } from "firebase/storage";
+import {v4}  from "uuid";
+import { getFirestore } from "firebase/firestore";
 
 //getting elems form dom
 const forms_section = document.getElementById('forms_section');
 const form_step_1 = document.querySelector('#form_step_1');
 const form_step_2 = document.querySelector('#form_step_2');
-let currentStep = 1;
-let imagesObj;
+let product_data_obj = {};
+let imagesObj = {};
 const prev_step_btn = document.querySelector('#prev_step_btn');
 const form_msg_display = document.querySelector('#form__msg-display');
 
@@ -24,8 +32,9 @@ const firebaseConfig = {
     measurementId: "G-LXN5WG6V2S",
     storageBucket: "https://arux-24899.appspot.com/"
 };
-  
+
 const app = initializeApp(firebaseConfig)
+  
 
 //functions
 const hasEmptySrc = (dropAreaArr) => {
@@ -65,113 +74,87 @@ const updateSteps = (direction='next')=>{
     currentActiveForm.classList.remove("currentActiveForm");
     currentActiveForm.classList.add("hidden");
 }
-const gatherDataOnlocalStorage = (storageKey, key, value)=>{
-    const obj = localStorage.getItem(storageKey) == null ? {} : JSON.parse(localStorage.getItem(storageKey));
 
-    if(obj){
-        obj[key] = value;
-    }
-    const stringifiedObj = JSON.stringify(obj)
-    localStorage.setItem(storageKey, stringifiedObj)
+// Function to upload image and get url
+const uploadImgToImgBB = async function (file) {
+    const apiKey = '5d7edd7bd9bc6cbfa53d30c3e83e3970';
+    const formData = new FormData();
+    formData.append('image', file);
+    formData.append('key', apiKey);
 
-}
-const prepareToUpload = (imgsObj)=>{
-    const storage = getStorage();
-    const storageRef = ref(storage, 'Products');
-    let imgNum = 0;
-    const imgFileArr = [
-        imgsObj.primary_img,
-        imgsObj.secondary_imgs[0],
-        imgsObj.secondary_imgs[1],
-    ];
-    const uploadedImgsURL = {secondary_imgs:[]};
-
-    imgFileArr.forEach(file =>{
-        file++;
-        uploadBytes(storageRef, file).then((snapshot) => {
-          const uploadTask = uploadBytesResumable(storageRef, file);
-          uploadTask.on(
-            "state_changed",
-            (snapshot) => {
-              const progress =
-                (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-
-                forms_section.innerHTML = `
-                <div class ="loader-con">
-                    Uploading image please wait...
-                    <h3> ${imgNum}/${imgFileArr.length} | ${progress}% done </h3>
-                    <div class="loader"></div>
-                </div>
-                `
-            },
-            (error) => {
-                forms_section.innerHTML = `
-                <div class ="loader-con">
-                    error: ${error} <br>
-                    <h3> Task failed! <a href="">Please try again.</a></h3>
-
-                </div>
-                `
-            },
-            () => {
-              getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                if(imgNum < 2){
-                    uploadedImgsURL[primary_img] = downloadURL
-                }else{
-                    uploadedImgsURL.secondary_imgs.push(downloadURL)
-                }
-
-              });
-            }
-          );
+    try {
+        const response = await fetch('https://api.imgbb.com/1/upload', {
+            method: 'POST',
+            body: formData,
         });
-        
-        localStorage.setItem("uploadedImgsUrls", JSON.stringify(uploadedImgsURL));
-    })
+
+        if (!response.ok) {
+            throw new Error('Failed to upload image');
+        } else {
+            const data = await response.json();
+            return data.data.url; // URL of the uploaded image
+        }
+    } catch (error) {
+        console.error('Error uploading image:', error);
+        return null;
+    }
+};
+
+const storeProductToDB = (productDataObj, productImgObj)=>{
+    const combinedData = {...productDataObj};
+
+    combinedData.primary_img = uploadImgToImgBB(productImgObj[primary_img]);
+
+    combinedData.secondary_1 = uploadImgToImgBB(productImgObj.secondary_img_0);
+
+    combinedData.secondary_2 = uploadImgToImgBB(productImgObj.secondary_img_1);
+
+    console.log(combinedData);
+
 }
 
 
 //event listners
 form_step_1.addEventListener('submit',(e)=>{
     e.preventDefault();
-    const dropAreas = [...e.target.querySelectorAll('.image-upload')]
-
-     if(!hasEmptySrc(dropAreas)){
+    const dropAreas = [...e.target.querySelectorAll('.image-upload')];
+    
+    if(!hasEmptySrc(dropAreas)){
+        
         const toStoreElems = [...e.target.querySelectorAll('[data-identification_name]')];
-
         const primary_img = e.target.querySelector('#primary_img').src;
-
         const secondary_imgs = [...e.target.querySelectorAll('.secondary_image_preview')]
 
-        for (let i = 0; i < secondary_imgs.length; i++) {
-            secondary_imgs[i] = secondary_imgs[i].src
-            
+        const images = {
+            primary_img
         }
 
-        const images = {
-            primary_img,
-            secondary_imgs,
+        for (let i = 0; i < secondary_imgs.length; i++) {
+            images[`secondary_img_${i}`] = secondary_imgs[i].src       
         }
+
+
         imagesObj = {...images}
         
         toStoreElems.forEach((elem)=>{
             const feildName = elem.dataset.identification_name
-            gatherDataOnlocalStorage("product_to_be_Added", feildName, elem.value);
+            product_data_obj[feildName] = elem.value;
         })
+        console.log(``,toStoreElems, images, imagesObj)
         updateSteps()
     }
-
 })
 
 form_step_2.addEventListener('submit',(e)=>{
     e.preventDefault();
     const toStoreElems = [...e.target.querySelectorAll('[data-identification_name]')];
+
     toStoreElems.forEach((elem)=>{
         const feildName = elem.dataset.identification_name
-        gatherDataOnlocalStorage("product_to_be_Added", feildName, elem.value);
+        product_data_obj[feildName] = elem.value;
     })
 
-    prepareToUpload(imagesObj);
+    storeProductToDB(product_data_obj,imagesObj)
 })
 
 window.onload = ()=>{
