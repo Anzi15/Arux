73da61cbf91b248bfae5e87819a09af002500b83
Lemme.code <djam4343@gmail.com>
2024-05-02 09:57:54 +0500

    @Core/features product details page working fine

--- src/js/individual-product.js
+++ src/js/individual-product.js
@@ -1,16 +1,20 @@
 'use strict';
 
-import { doc } from 'firebase/firestore';
 //*essential imports
 import {
     getFirestoreDocument,
+    getFewFirestoreDocs
 } from './admin-modules';
-import {getParamFromUrl} from './general-modules'
+import {getParamFromUrl} from './general-modules';
+import {addProductToDom, removeCertainClassedElemsFromDom} from './client_side-modules';
 
 //*variables and dom elements
 const quantityInpElem = document.getElementById('quantityInpElem');
 const quantitySubtractBtn = document.getElementById('subtractBtn');
 const quantityPlusBtn = document.getElementById('plusBtn');
+const recomendedProductsCon = document.getElementById('recomended-products-con');
+const productNavTreeProductName = document.getElementById('product-tree-this-product-name');
+
 
 let docID = getParamFromUrl("id");
 let productDoc;
@@ -28,7 +32,9 @@ const domImgs = {
 (async ()=>{
     if(docID ==null || docID == undefined) return;
     productDoc = await getFirestoreDocument("Products",docID);
-    console.log(productDoc)
+
+    productNavTreeProductName.innerHTML = productDoc.title;
+    productNavTreeProductName.classList.remove("skeleton-loading")
 
     for(const elem in domDetails){
         domDetails[elem].innerHTML = productDoc[elem];
@@ -37,14 +43,31 @@ const domImgs = {
 
         domDetails[elem].classList.remove("skeleton-loading")
     }
+
     for(const elem in domImgs){
         domImgs[elem].src = productDoc[elem];
         domImgs[elem].alt = productDoc[elem];
     }
+
+    const recomendedProduct = await getFewFirestoreDocs("Products",5);
+    let productsAddedToDom = 0;
+    recomendedProduct.forEach(product => {
+        if(productsAddedToDom<4 && product.data.title !== productDoc.title){
+            addProductToDom(recomendedProductsCon,product.data,product.id);
+            productsAddedToDom++
+        }
+    });
+    removeCertainClassedElemsFromDom(recomendedProductsCon, "placeolder-products")
 })()
 
 //*eventlistners
 quantityPlusBtn.onclick = ()=>{quantityInpElem.value++}
 quantitySubtractBtn.onclick = ()=>{
     quantityInpElem.value < 2 ? quantityInpElem.value=1 : quantityInpElem.value--
+}
+
+for(const elem in domImgs){
+    domImgs[elem].parentElement.addEventListener("click",()=>{
+        domImgs[elem].parentElement.classList.toggle("overlay-img-preview")
+    })
 }
